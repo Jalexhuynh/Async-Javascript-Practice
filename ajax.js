c6162755cdc94afd74ada7045a32748813099858
Justin Huynh <j.alexhuynh@gmail.com>
2018-07-25 18:03:00 -0700

    Add fetch API notes.

--- ajax.js
+++ ajax.js
@@ -13,3 +13,32 @@
 
 	XHR.open("GET", "https://api.github.com/zen");
 	XHR.send();
+
+// 2. ========== FETCH API ========== //
+
+// EXAMPLE 2.1: Example Fetch Request
+	fetch(url)
+	.then(function(res) {
+		console.log(res);
+	})
+	.catch(function(error) {
+		console.log(error);
+	});
+
+// EXAMPLE 2.2: Parsing JSON with Fetch with Options
+	fetch(url, {
+		method: 'POST',
+		body: JSON.stringify({
+			name: 'Name',
+			login: 'Username'
+		})
+	})
+	.then(function(res) {
+		return res.json();
+	})
+	.then(function(data) {
+		console.log(data);
+	})
+	.catch(function() {
+		console.log("Problem!");
+	});
